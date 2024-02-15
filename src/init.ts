9f14b94b02272c1abc942f89dd188fde82aee791
BuildTools <unconfigured@null.spigotmc.org>
2024-02-15 12:30:58 +0100

    initial commit

---
+++ src/init.ts
@@ -0,0 +1,9 @@
+import * as os from "os";
+import * as keyboard from "keyboard";
+import * as computer from "computer";
+import * as component from "component";
+import * as colors from "colors";
+import * as note from "note";
+import * as thread from "thread";
+import * as event from "event";
+
